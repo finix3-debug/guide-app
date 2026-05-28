4465961a8cbbda545f36a7c4d0b78e18a65ea30c
finix3-debug <w.finix3@gmail.com>
2026-05-28 13:38:58 +0100

    Update MainScreen.tsx

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -38,12 +38,12 @@ const MainScreen = () => {
 
             <GlassButton onClick={() => navigate("/protocoles")}>
               <Heart size={20} className="text-cyan-300" />
-              <span>Protocoles d'Examen</span>
+              <span>Anatomie Cardio Vasculaire & L'Angioscanner thoracique</span>
             </GlassButton>
 
             <GlassButton onClick={() => navigate("/contact")}>
               <Syringe size={20} className="text-cyan-300" />
-              <span>Infos & Contact</span>
+              <span>L’injecteur automatique & Produit de contraste</span>
             </GlassButton>
           </motion.div>
         </div>
